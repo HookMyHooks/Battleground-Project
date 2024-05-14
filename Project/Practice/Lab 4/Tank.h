898c14d5fb88b1bd3363c8efd5dc283b7afd9df4
HookMyHooks <liviunedelcu10@gmail.com>
2024-05-14 23:44:39 +0300

    Added new files - new building Changed location of the tank for easier finding

---
+++ Project/Practice/Lab 4/Tank.h
@@ -0,0 +1,70 @@
+#ifndef TANK_CLASS_H
+#define TANK_CLASS_H
+
+#include<json.h>
+#include <unordered_set>
+#include "Mesh.h"
+
+using json = nlohmann::json;
+
+
+//Sorry, not enought time to implement callbacks => copied class of Model
+
+class Tank
+{
+public:
+	// Loads in a model from a file and stores tha information in 'data', 'JSON', and 'file'
+	Tank(const char* file);
+
+	void Draw(Shader& shader, Camera& camera, glm::vec3 translation, glm::quat rotation, glm::vec3 scale);
+
+private:
+
+
+	//fixing unloading meshes
+	std::unordered_set<unsigned int> visitedNodes;
+
+	// Variables for easy access
+	const char* file;
+	std::vector<unsigned char> data;
+	json JSON;
+
+	// All the meshes and transformations
+	std::vector<Mesh> meshes;
+	std::vector<glm::vec3> translationsMeshes;
+	std::vector<glm::quat> rotationsMeshes;
+	std::vector<glm::vec3> scalesMeshes;
+	std::vector<glm::mat4> matricesMeshes;
+
+	// Prevents textures from being loaded twice
+	std::vector<std::string> loadedTexName;
+	std::vector<Texture> loadedTex;
+
+	// Loads a single mesh by its index
+	void loadMesh(unsigned int indMesh);
+
+	// Traverses a node recursively, so it essentially traverses all connected nodes
+	void traverseNode(unsigned int nextNode, glm::mat4 matrix = glm::mat4(1.0f));
+
+	// Gets the binary data from a file
+	std::vector<unsigned char> getData();
+	// Interprets the binary data into floats, indices, and textures
+	std::vector<float> getFloats(json accessor);
+	std::vector<GLuint> getIndices(json accessor);
+	std::vector<Texture> getTextures();
+
+	// Assembles all the floats into vertices
+	std::vector<Vertex> assembleVertices
+	(
+		std::vector<glm::vec3> positions,
+		std::vector<glm::vec3> normals,
+		std::vector<glm::vec2> texUVs
+	);
+
+	// Helps with the assembly from above by grouping floats
+	std::vector<glm::vec2> groupFloatsVec2(std::vector<float> floatVec);
+	std::vector<glm::vec3> groupFloatsVec3(std::vector<float> floatVec);
+	std::vector<glm::vec4> groupFloatsVec4(std::vector<float> floatVec);
+};
+
+#endif
