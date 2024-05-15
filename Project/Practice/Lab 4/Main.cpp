7d8517123163b06c0e883c10ce6b8cbae3dbc4ae
andreeamacsim <mlaviniaandreea@yahoo.com>
2024-05-15 11:31:59 +0300

    More normal night skybox

--- Project/Practice/Lab 4/Main.cpp
+++ Project/Practice/Lab 4/Main.cpp
@@ -62,12 +62,12 @@ std::vector<std::string> facesDay
 std::vector<std::string>facesNight
 {
 
-	"Skybox_night\\sky.jpg",
-	"Skybox_night\\sky.jpg",
-	"Skybox_night\\sky.jpg",
-	"Skybox_night\\sky.jpg",
-	"Skybox_night\\sky.jpg",
-	"Skybox_night\\sky.jpg"
+	"Skybox_night\\sky2.png",
+	"Skybox_night\\sky2.png",
+	"Skybox_night\\sky2.png",
+	"Skybox_night\\sky2.png",
+	"Skybox_night\\sky2.png",
+	"Skybox_night\\sky2.png"
 	
 	/*"Skybox_night\\left.jpg",
 	"Skybox_night\\left.jpg",
